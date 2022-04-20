dbd7f1fdb2cba35f6370d37a54edbd4988a70e7e
Ognjen Lazic <laogdo@gmail.com>
2022-04-20 20:58:48 +0200

    Make stepper a class and let it control the state of the motor and its position

--- app.py
+++ app.py
@@ -8,77 +8,60 @@ if platform.system() == 'Darwin':
     sys.modules['RPi'] = fake_rpi.RPi  # Fake RPi
     sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO  # Fake GPIO
 
-from threading import Thread
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from sensor import get_limit_switch_state
-from stepper import STEPS_PER_MM_DEFAULT, motor_move, motor_stop, DEFAULT_STEP_TYPE, is_set_type_valid, speed_to_delay, \
-    DEFAULT_STEP_DELAY
+from src.stepper import MotorState
+from src.stepper import STEPS_PER_MM_DEFAULT, DEFAULT_STEP_TYPE, is_set_type_valid, speed_to_delay, DEFAULT_STEP_DELAY
 
 app = Flask(__name__)
 CORS(app)
 
-global motor_thread
+motor_control = MotorState(motor_position=0, is_motor_moving=False)
 
 
 @app.route("/")
 def main():
-    response = {"message": "Server is running"}
+    response = {"message": "Motor controlling server is running"}
     return jsonify(response), 200
 
 
-@app.route('/is-limit-switch-on', methods=['GET'])
-def is_limit_switch_on():
-    limit_switch_state = bool(get_limit_switch_state())
-    response = {"isLimitSwitchOn": limit_switch_state}
+@app.route('/is-motor-at-start', methods=['GET'])
+def is_at_start():
+    response = {"isLimitSwitchOn": motor_control.is_motor_at_start()}
     return jsonify(response), 200
 
 
 @app.route('/is-moving', methods=['GET'])
 def is_moving():
-    global motor_thread
-    try:
-        if motor_thread.is_alive():
-            response = {"message": "Motor is moving", "motorIsMoving": True}
-            return jsonify(response), 200
-        else:
-            response = {"message": "Motor is idle", "motorIsMoving": False}
-            return jsonify(response), 200
-    except Exception:
+    if motor_control.is_motor_moving:
+        response = {"message": "Motor is moving", "motorIsMoving": True}
+        return jsonify(response), 200
+    else:
         response = {"message": "Motor is idle", "motorIsMoving": False}
         return jsonify(response), 200
 
 
-@app.route('/stop', methods=['POST'])
-def stop():
-    motor_stop()
-    response = {"message": "Motor is stopped"}
-    return jsonify(response), 200
-
-
 @app.route('/move-to-start', methods=['POST'])
 def move_to_start():
-    motor_stop()
-    response = {"message": "Motor sent to 2 m back to root"}
-    global motor_thread
-    motor_thread = Thread(
-        target=lambda: motor_move(-200, STEPS_PER_MM_DEFAULT, DEFAULT_STEP_TYPE, DEFAULT_STEP_DELAY))
-    motor_thread.start()
-    return jsonify(response), 200
+    if motor_control.is_motor_moving:
+        response = {"message": "Motor is already moving"}
+        return jsonify(response), 409
+    else:
+        response = {"message": "Motor sent back to start"}
+        motor_control.move_motor_to_start()
+        return jsonify(response), 200
 
 
 @app.route('/move', methods=['POST'])
 def move():
-    motor_stop()
     data = request.json
     steps_per_mm = data.get("stepsPerMm", STEPS_PER_MM_DEFAULT)
     distance_to_move = data.get("distanceToMove", 0)
     step_type = data.get("stepType", DEFAULT_STEP_TYPE)
 
     speed = data.get("speed", None)
-    step_delay = data.get("stepDelay", None)
+    step_delay = data.get("stepDelay", DEFAULT_STEP_DELAY)
 
     if not is_set_type_valid(step_type):
         response = {"message": "Step type is not valid"}
@@ -94,12 +77,13 @@ def move():
             elif step_delay:
                 step_delay = step_delay
 
-            response = {"message": "Motor sent to %s with steps %s" % (distance_to_move, steps_per_mm)}
-            global motor_thread
-            motor_thread = Thread(
-                target=lambda: motor_move(distance_to_move, steps_per_mm, step_type, step_delay))
-            motor_thread.start()
-            return jsonify(response), 200
+            if motor_control.is_motor_moving:
+                response = {"message": "Motor is already moving"}
+                return jsonify(response), 409
+            else:
+                response = {"message": "Motor sent to %s with steps %s" % (distance_to_move, steps_per_mm)}
+                motor_control.move_motor_to_distance(distance_to_move, steps_per_mm, step_type, step_delay)
+                return jsonify(response), 200
         else:
             response = {"message", "Distance not provided"}
             return jsonify(response), 400
---
+++ src/stepper.py
@@ -0,0 +1,97 @@
+from threading import Thread
+
+import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+
+from src.sensor import get_limit_switch_state
+
+direction = 22  # Direction (DIR) GPIO Pin
+step = 23  # Step GPIO Pin
+EN_pin = 24  # enable pin (LOW to enable)
+
+STEPS_PER_MM_DEFAULT = 19.0375
+
+DEFAULT_STEP_TYPE = 'Full'
+STEP_TYPES = ['Full', 'Half', "1/4", '1/8', '1/16', '1/32']
+
+DEFAULT_STEP_DELAY = .0005
+DEFAULT_SPEED = 99
+SEED_TO_DELAY_DIFFERENCE = 100000
+
+MIN_DELAY = 0.00099
+MAX_DELAY = 0.00001
+
+
+def speed_to_delay(speed):
+    if 1 <= speed <= 99:
+        return speed / SEED_TO_DELAY_DIFFERENCE
+    else:
+        raise Exception('Speed needs to be in limits from 1 to 99')
+
+
+# Declare a instance of class pass GPIO pins numbers and the motor type
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(EN_pin, GPIO.OUT)  # set enable pin as output
+motor = RpiMotorLib.A4988Nema(direction, step, (21, 21, 21), "DRV8825")
+
+
+def is_set_type_valid(step_type):
+    return step_type in STEP_TYPES
+
+
+def move_motor_to_steps(steps, step_type, step_delay, movement_done):
+    try:
+        # pull enable to low to enable motor
+        GPIO.output(EN_pin, GPIO.LOW)
+
+        motor.motor_go(steps > 0,  # True=Clockwise, False=Counter-Clockwise
+                       step_type,  # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                       abs(steps),  # number of steps
+                       step_delay,  # step delay [sec]
+                       False,  # True = print verbose output
+                       .05)  # initial delay [sec]
+        movement_done()
+    except Exception as E:
+        print("MOTOR FAILED TO MOVE")
+        print(E)
+        movement_done()
+        pass
+
+
+def stop_motor():
+    try:
+        # RPi.GPIO.output(EN_pin, RPi.GPIO.HIGH)
+        motor.motor_stop()
+    except Exception as E:
+        print("MOTOR FAILED TO STOP")
+        print(E)
+        pass
+
+
+class MotorState:
+    def __init__(self, motor_position, is_motor_moving):
+        self.motor_position = motor_position
+        self.is_motor_moving = is_motor_moving
+
+    def motor_movement_complete(self, moved_steps):
+        self.motor_position = moved_steps
+        self.is_motor_moving = False
+
+    def is_motor_at_start(self):
+        return self.motor_position is 0 and bool(get_limit_switch_state())
+
+    def move_motor_to_distance(self, distance_to_move, steps_per_mm, step_type, step_delay):
+        self.is_motor_moving = True
+        steps_to_move = int((float(distance_to_move) * 10) * int(steps_per_mm))
+        steps_to_move_from_current_position = self.motor_position - steps_to_move
+        motor_thread = Thread(
+            target=lambda: move_motor_to_steps(steps_to_move_from_current_position, step_type, step_delay,
+                                               lambda: self.motor_movement_complete(steps_to_move)))
+        motor_thread.start()
+
+    def move_motor_to_start(self):
+        self.is_motor_moving = True
+        motor_thread = Thread(
+            target=lambda: move_motor_to_steps(-10000, DEFAULT_STEP_TYPE, DEFAULT_STEP_DELAY,
+                                               lambda: self.motor_movement_complete(0)))
+        motor_thread.start()
